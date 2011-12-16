9063eeb7a3032d3b22607374feec00d499789a4a
alexbwilliams <alex.bransbywilliams@gmail.com>
2011-12-16 11:11:53 +0000

    Step 2

--- palindrome-tests.js
+++ palindrome-tests.js
@@ -6,4 +6,9 @@ test("p", function() {
 
 test("isPalindrome", function() {
     ok(q.isF(p.isPalindrome), "is a function");
+    equal(false, p.isPalindrome(1234), "rejects ints");
+    equal(false, p.isPalindrome(12.34), "rejects floats");   
+    equal(false, p.isPalindrome({}), "rejects objs");
+    equal(false, p.isPalindrome(function(){}), "rejects functions");
+    
 });
--- palindrome.js
+++ palindrome.js
@@ -1,4 +1,12 @@
 var p = function(p){
-
+    p.isPalindrome = function(s, x1, x2)
+    {
+        if (q.isS(s) === false) {
+            return false;
+        }
+        
+        return true;
+    
+    }
     return p;
 }(p || {});
